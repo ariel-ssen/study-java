4188034fe388c7774b3bf97efa77667190134e34
조세은 <joseeun@joseeun-ui-MacBookPro.local>
2025-02-10 18:42:38 +0900

    20250210

---
+++ src/CommentJava.java
@@ -0,0 +1,16 @@
+public class CommentJava {
+
+    //psvm Enter
+    public static void main(String[] args) {
+        System.out.println("hello java1"); // hello java1을 출력합니다.
+        // 한줄 주석
+        // System.out.println("hello java2");
+//        command + ? 단축키
+
+        /* "/* 하면 여러줄 주석이 가능함"
+        System.out.println("hello java3");
+        System.out.println("hello java3");
+         */
+
+    }
+}
